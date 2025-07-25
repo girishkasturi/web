34c719b2c0ba4ec3bcc99b33d25e2b8036d18383
girish <girishkasturi1234@gmail.com>
2025-07-25 00:04:35 -0400

    update

---
+++ src/main/java/com/sark/sark/website/SarkWebsiteApplication.java
@@ -0,0 +1,12 @@
+package com.sark.sark.website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SarkWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SarkWebsiteApplication.class, args);
+    }
+}
---
+++ src/test/java/com/sark/sark/website/SarkWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sark.sark.website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SarkWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
